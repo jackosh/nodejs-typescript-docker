aa9f36288464b5b42936b851b938d51679dce2bf
root@rrybak.com <ruslan.rybak@gmail.com>
2020-06-07 13:22:08 +0200

    testing ts-node test-set.ts. running typescript direct

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,19 +9,42 @@ enum ObjectLabels {
     Electronics = "Electronics",
     Tool = "Tool",
     Accessoire = "Accessoire",
+    Paper = "Paper",
+    Document = "Document"
 }
 
 class InventoryObject {
     objectId: number;
     name: string;
-    constructor (objectId: number, name: string) {
+    private labels: Set<ObjectLabels>
+    constructor (objectId: number, name: string, labels: Set<ObjectLabels>) {
         this.objectId = objectId
         this.name = name
+        this.labels = labels
+    }
+    getKeys() {
+        return this.labels.keys()
+    }
+    getLabels() {
+        return this.labels.entries()
+    }
+    addLabel(label: ObjectLabels) {
+        this.labels.add(label)
     }
 }
 
-let lgG6 = new InventoryObject(0, 'Lg G6');
+let lgG6 = new InventoryObject(0, 'Lg G6', new Set([ObjectLabels.Electronics]));
+console.log(lgG6.getLabels())
+console.log(lgG6.getKeys())
+lgG6.addLabel(ObjectLabels.Tool)
+let objectString = JSON.stringify(lgG6)
+console.log('jsobject')
 console.log(lgG6);
+console.log('stringify -> object string:')
+console.log(objectString);
+console.log('parse -> parsed object:')
+let obj: InventoryObject = JSON.parse(objectString)
+console.log(obj)
 /*
 Objects
 
---
+++ server/test-set.ts
@@ -0,0 +1,9 @@
+let tSet = new Set()
+tSet.add(3);
+tSet.add("one");
+console.log(tSet.has('one'));
+// for (let k of tSet.keys()) {
+//     console.log(k)
+// }
+// console.log(tSet.entries());
+// tSet.keys().forEach((k) => console.log(k))
